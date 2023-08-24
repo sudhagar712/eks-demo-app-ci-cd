8abd3de552d7c9dee59df0375bb8b88d723320d7
sudhagar712 <128031782+sudhagar712@users.noreply.github.com>
2023-08-24 17:35:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 9000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! - welcome to icu program project 2 - perumbakkam ')
+  res.send('Hello World! - welcome to icu program project 2 ')
 })
 
 app.listen(port, () => {
